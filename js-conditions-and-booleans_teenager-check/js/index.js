1c982c9f5bf2823d6f7dafdd53132ddbaae6b173
Arne Witteler <witteler@arnenoah.de>
2024-03-23 14:06:11 +0100

    add functionality to teenager check

--- js-conditions-and-booleans_teenager-check/js/index.js
+++ js-conditions-and-booleans_teenager-check/js/index.js
@@ -3,11 +3,16 @@ const button = document.querySelector('[data-js="age-button"]');
 const output = document.querySelector('[data-js="age-output"]');
 
 button.addEventListener("click", () => {
-  // Number(x) converts the string from the input to a number
-  // Beware: input values are *always* strings!
   const age = Number(input.value);
-
-  // Exercise:
-  // Use conditions and the logical AND operator to write
-  // "You are a teen." or "You are not a teen." into the output.
+  if (age > 10 && age < 20) {
+    output.textContent = "You are a teen";
+  } else {
+    output.textContent = "You are not a teen";
+  }
 });
+
+// Number(x) converts the string from the input to a number
+// Beware: input values are *always* strings!
+// Exercise:
+// Use conditions and the logical AND operator to write
+// "You are a teen." or "You are not a teen." into the output.
