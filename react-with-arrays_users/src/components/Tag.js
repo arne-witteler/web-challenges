10dc628820aaf8a6f8387f13a24bf887c94a1a30
Arne Witteler <witteler@arnenoah.de>
2024-03-27 12:43:32 +0100

    task 3: refactor tag component

--- react-with-arrays_users/src/components/Tag.js
+++ react-with-arrays_users/src/components/Tag.js
@@ -1,5 +1,6 @@
 import "./Tag.css";
 
 export default function Tag({ tag }) {
-  return <li className="tag">{tag}</li>;
+  const tagClass = tag === "admin" ? " tag tag--highlight" : "tag";
+  return <li className={tagClass}>{tag}</li>;
 }
