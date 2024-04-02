b04d9cff9490bd8cb8f1a08d99dbc4ed96d4c56a
Arne Witteler <witteler@arnenoah.de>
2024-04-02 11:52:35 +0200

    add functionality to next and previous button

---
+++ react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export default function PokemonList() {
+  const [pokemon, setPokemon] = useState([]);
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    async function loadPokemon() {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?offset=${offset}`
+        );
+        const data = await response.json();
+        setPokemon(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    loadPokemon();
+  }, [offset]);
+
+  return (
+    <main>
+      <button
+        type="button"
+        hidden={offset === 0}
+        onClick={() => setOffset((prevOffset) => prevOffset - 20)}
+      >
+        Previous Page
+      </button>
+      <button
+        type="button"
+        onClick={() => setOffset((prevOffset) => prevOffset + 20)}
+      >
+        Next Page
+      </button>
+      <ul>
+        {pokemon.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
