3d3f936ae7313d00d471b6ae6eafdd72a51338fe
Arne Witteler <witteler@arnenoah.de>
2024-04-10 15:50:48 +0200

    add bonus task 2

--- react-styled-components_box/components/BoxWithStyledComponents/BoxWithStyledComponents.js
+++ react-styled-components_box/components/BoxWithStyledComponents/BoxWithStyledComponents.js
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 
-const StyledBox = styled.div`
+// const StyledBox = styled.div`
+//   width: 100px;
+//   height: 100px;
+//   margin: 2rem;
+//   background-color: ${(props) => (props.$isBlack ? "black" : "green")};
+//   &:hover {
+//     background-color: red;
+//   }
+// `;
+
+// export default function BoxWithStyledComponents({ $isBlack }) {
+//   return <StyledBox $isBlack={$isBlack} />;
+// }
+
+const BoxWithStyledComponents = styled.div`
   width: 100px;
   height: 100px;
   margin: 2rem;
@@ -10,6 +24,4 @@ const StyledBox = styled.div`
   }
 `;
 
-export default function BoxWithStyledComponents({ $isBlack }) {
-  return <StyledBox $isBlack={$isBlack} />;
-}
+export default BoxWithStyledComponents;
