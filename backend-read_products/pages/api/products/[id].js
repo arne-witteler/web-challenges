bd062d92a3de0e7806f1c1e3b60cb8cf6894b6b6
Arne Witteler <witteler@arnenoah.de>
2024-04-18 16:07:17 +0200

    add database, connect to database, no bonus

---
+++ backend-read_products/pages/api/products/[id].js
@@ -0,0 +1,16 @@
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  if (request.method === "GET") {
+    const product = Product.findById(id);
+
+    if (!joke) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+    response.status(200).json(product);
+  }
+}
