c52914ac648d7c08517911927d81f2ea7ac60296
Arne Witteler <witteler@arnenoah.de>
2024-04-19 16:39:24 +0200

    make ProductForm reuseable, send put request

--- backend-update-and-delete_products/components/Product/index.js
+++ backend-update-and-delete_products/components/Product/index.js
@@ -3,30 +3,71 @@ import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
 import Comments from "../Comments";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
+import { StyledButton } from "../Button/Button.styled";
 
 export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+      mutate();
+      setIsEditMode(false);
+    }
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
   if (!data) {
-    return;
+    return null;
   }
 
   return (
     <ProductCard>
-      <h2>{data.name}</h2>
-      <p>Description: {data.description}</p>
-      <p>
-        Price: {data.price} {data.currency}
-      </p>
-      {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
-      <StyledLink href="/">Back to all</StyledLink>
+      {isEditMode ? (
+        <ProductForm
+          onSubmit={handleEditProduct}
+          formHeading="Edit Product Details"
+        />
+      ) : (
+        <>
+          <h2>{data.name}</h2>
+          <p>Description: {data.description}</p>
+          <p>
+            Price: {data.price} {data.currency}
+          </p>
+          {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+          <StyledLink href="/">Back to all</StyledLink>
+          <StyledButton
+            type="button"
+            onClick={() => {
+              setIsEditMode(!isEditMode);
+            }}
+          >
+            {isEditMode ? "Cancel" : "Edit"}
+          </StyledButton>
+        </>
+      )}
     </ProductCard>
   );
 }
--- backend-update-and-delete_products/pages/api/products/[id].js
+++ backend-update-and-delete_products/pages/api/products/[id].js
@@ -14,4 +14,10 @@ export default async function handler(request, response) {
 
     response.status(200).json(product);
   }
+  if (request.method === "PUT") {
+    const updatedProduct = request.body;
+    await Product.findByIdAndUpdate(id, updatedProduct);
+    response.status(200).json({ status: "Product successfully updated." });
+    return;
+  }
 }
