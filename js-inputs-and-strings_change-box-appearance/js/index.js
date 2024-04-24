934e3122a758014f1f4c95fef5505972220b4a61
Arne Witteler <witteler@arnenoah.de>
2024-04-24 12:55:51 +0200

    add event listeners and functionality to all sliders

--- js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings_change-box-appearance/js/index.js
@@ -2,10 +2,10 @@
 
 Change the appearance of the box according to the values of the sliders (color, border radius, rotation)
 
-– [ ] Add an event listener for each input element.
-– [ ] When the slider control of inputColor is moved, change the background color of the box. Hint: You need to work with the hsl() functional notation for colors.
-– [ ] When the slider control of inputRadius is moved, change the border radius of the box. If the control reaches the right end, make the box look like a circle.
-– [ ] When the slider control of inputRotation is moved, rotate the box. Hint: You may need to work with the CSS property "transform" and a value that contains "rotate" and "deg".
+– [x] Add an event listener for each input element.
+– [x] When the slider control of inputColor is moved, change the background color of the box. Hint: You need to work with the hsl() functional notation for colors.
+– [x] When the slider control of inputRadius is moved, change the border radius of the box. If the control reaches the right end, make the box look like a circle.
+– [x] When the slider control of inputRotation is moved, rotate the box. Hint: You may need to work with the CSS property "transform" and a value that contains "rotate" and "deg".
 
 Hint: You can work with the property "style", just like you worked with the property "classList".
 
@@ -16,3 +16,20 @@ For further information check MDN:
 - about CSS declarations with "style": https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
 
 */
+
+const boxElement = document.querySelector('[data-js="box"]');
+const colorSlider = document.querySelector('[data-js="input-color"]');
+const borderRadiusSlider = document.querySelector('[data-js="input-radius"]');
+const rotationSlider = document.querySelector('[data-js="input-rotation"]');
+
+colorSlider.addEventListener("input", () => {
+  boxElement.style.backgroundColor = `hsl(${colorSlider.value}, 80%, 90%)`;
+});
+
+borderRadiusSlider.addEventListener("input", () => {
+  boxElement.style.borderRadius = borderRadiusSlider.value + "px";
+});
+
+rotationSlider.addEventListener("input", () => {
+  boxElement.style.transform = `rotate(${rotationSlider.value}deg)`;
+});
